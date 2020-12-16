f09e73ead803c973f1a6b2b54fb62bc67e516074
EC2 Default User <ec2-user@ip-172-31-16-38.ec2.internal>
2020-12-16 03:33:09 +0000

    add new file

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = (function () {
+  searchWord = function(){
+    var searchText = $(this).val(), // 検索ボックスに入力された値
+        targetText;
+
+    $('.target-area li').each(function() {
+      targetText = $(this).text();
+
+      // 検索対象となるリストに入力された文字列が存在するかどうかを判断
+      if (targetText.indexOf(searchText) != -1) {
+        $(this).removeClass('hidden');
+      } else {
+        $(this).addClass('hidden');
+      }
+    });
+  };
+
+  // searchWordの実行
+  $('#search-text').on('input', searchWord);
+});
